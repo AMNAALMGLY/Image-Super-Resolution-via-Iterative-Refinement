0a0e2afad885f36e96ab56790f81067da2ff65df
AMNAALMGLY <mnmnalmagly@gmail.com>
2023-01-12 22:07:35 -0800

    fix model bug

--- model/networks.py
+++ model/networks.py
@@ -86,9 +86,9 @@ def init_weights(net, init_type='kaiming', scale=1, std=0.02):
 def define_G(opt):
     model_opt = opt['model']
     if model_opt['which_model_G'] == 'ddpm':
-        from .ddpm_modules import diffusion, unet,UMaskedAutoencoderViT
+        from .ddpm_modules import diffusion, unet
     elif model_opt['which_model_G'] == 'sr3':
-        from .sr3_modules import diffusion, unet,UMaskedAutoencoderViT
+        from .sr3_modules import diffusion, unet
     if ('norm_groups' not in model_opt['unet']) or model_opt['unet']['norm_groups'] is None:
         model_opt['unet']['norm_groups']=32
     # model = unet.UNet(
@@ -102,7 +102,7 @@ def define_G(opt):
     #     dropout=model_opt['unet']['dropout'],
     #     image_size=model_opt['diffusion']['image_size']
     # )
-    model=  UMaskedAutoencoderViT(
+    model=  unet.UMaskedAutoencoderViT(
             img_size=model_opt['umae']['img_size'],
             patch_size=model_opt['umae']['patch_size'],
             in_chans=model_opt['umae']['in_channel'],
